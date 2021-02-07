b1a77905bc5e27fa6d85b2bf4094d0425e6f487c
okdhrk <okdhrk1014@gmail.com>
2021-02-07 13:49:19 +0000

    0207

---
+++ Capter2/2.3.1.rb
@@ -0,0 +1,17 @@
+puts "こんにちは\nさようなら"
+puts
+
+puts 'こんにちは\nさようなら'
+puts
+
+name = 'Alice'
+puts "Hello, #{name}!"
+
+i = 10
+puts "#{i}は16進数にすると#{i.to_s(16)}です"
+
+name = 'Alice'
+puts 'Hello, #{name}!'
+
+name = 'Alice'
+puts 'Hello, ' + name + '!'
