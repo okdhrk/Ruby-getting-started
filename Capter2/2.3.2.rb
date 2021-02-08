1b1528d0ad766aa6e9d8285b4ef695089fb28fa6
okdhrk <okdhrk1014@gmail.com>
2021-02-08 13:11:25 +0000

    0208

---
+++ Capter2/2.3.2.rb
@@ -0,0 +1,12 @@
+puts 'ruby' == 'ruby'
+puts 'rubi' == 'Ruby'
+puts 'ruby' != 'perl'
+puts 'ruby' != 'ruby'
+
+puts 'a' < 'b'
+puts 'a' < 'A'
+puts 'a' > 'A'
+puts 'abc' < 'def'
+puts 'abc' < 'ab'
+puts 'abc' < 'abcd'
+puts 'あいうえお' < 'かきくけこ'
